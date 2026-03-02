6905a02518e1e02f59dba2c997c9b27c9d9234e2
MarkRufin-Zavhorodni <Mark.Rufin-Zavhorodnii@nangasystems.com>
2026-03-02 16:13:23 +0200

    f

--- bot/database/save_step.py
+++ bot/database/save_step.py
@@ -7,7 +7,7 @@ from bot.database.other_db import OtherSessionLocal
 async def save_step(telegram_id: int, step: str, username: str):
     async with OtherSessionLocal() as session:
         result = await session.execute(
-            select(UserProgress).filter_by(telegram_id=telegram_id, bot_name="hackbotruc")
+            select(UserProgress).filter_by(telegram_id=telegram_id, bot_name="hackbotazer")
         )
         progress = result.scalar_one_or_none()
 
@@ -17,7 +17,7 @@ async def save_step(telegram_id: int, step: str, username: str):
             progress = UserProgress(
                 telegram_id=telegram_id,
                 last_step=step,
-                bot_name="hackbotruc",
+                bot_name="hackbotazer",
                 username=username
             )
             session.add(progress)
